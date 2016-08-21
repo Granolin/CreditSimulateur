eb42b0dc1cca3d3deaf206b5684a2a3605263188
Granolin <granolin35@hotmail.com>
2016-08-22 00:23:20 +0200

    ajout d'un commentaire

--- CreditSimulateur/SimuTest/UnitTest1.cs
+++ CreditSimulateur/SimuTest/UnitTest1.cs
@@ -11,7 +11,7 @@ namespace SimuTest
         public void TestMethod1()
         {
             int maxEmprunt = Calc.GetEmprunt(30, 3000, 2);
-
+	    //On vérifie que c'est la bonne valeur
             Assert.AreEqual(maxEmprunt, 811646);
 
         }
